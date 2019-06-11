b5c5f4a939c3bce40ad89df2eef8b94e88f5e923
Alex <alexandre.rosselange@epitech.eu>
2019-06-11 16:16:31 +0200

    add registration

--- Api/src/Controller/AuthController.php
+++ Api/src/Controller/AuthController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use JMS\SerializerBundle\JMSSerializerBundle;
 
 
 class AuthController extends AbstractController
@@ -26,23 +27,26 @@ class AuthController extends AbstractController
 	}
 
 	/**
+	 * Extract email and password from POST request
+	 *  Manually check if not null
+	 *  
 	 * @Route("/register", name="api_register")
 	 */
 	public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, ValidatorInterface $validator): Response
 	{
 		$user = new User();
 
-		
-		$user->setEmail($request->request->get('email'));
-		$user->setPassword($request->request->get('password'));
-		
 		// method point after point
-		/* if(!$request->request->get('email')) {
+		if(!$request->request->get('email')) {
 			return $this->json([
-					'email' => $request->request->get('email'),
-					'password' => $request->request->get('password'),
-				]);
-		} */
+				'errors' => "email is required",
+			]);
+		}
+		if(!$request->request->get('password')) {
+			return $this->json([
+				'errors' => "password is required",
+			]);
+		}
 
 		// method try catch
 		/* try {
@@ -53,26 +57,28 @@ class AuthController extends AbstractController
 			return $this->json(['errors' => $errors, 'e' => $e]);
 		} */
 
-		$data = $this->get('jms_serializer')->serialize($user, 'json');
+		// $data = $this->get('jms_serializer')->serialize($user, 'json');
+
+		$user->setEmail($request->request->get('email'));
+		$plainPassword = $request->request->get('password');
+		$encoded = $passwordEncoder->encodePassword($user, $plainPassword);
+		$user->setPassword($encoded);
 
 		$errors = $validator->validate($user);
 
-		return new Response(
-			'<html><body><pre>'.print_r($request->request->all()).'</pre></body></html>'
-		);
-
-		/* if(count($errors) > 0) {
+		if(count($errors) > 0) {
 			return $this->json(['errors' => $errors]);
 		} else {
+			$entityManager = $this->getDoctrine()->getManager();
+			$entityManager->persist($user);
+			$entityManager->flush();
 			return $this->json([
 				'email' => $request->request->get('email'),
-				'password' => $request->request->get('password'),
+				'user_id' => $user->getId()
 			]);
-		} */
+		}
 
-		/* return new Response(
-		    '<html><body><pre>'.print_r($request->request->all()).'</pre></body></html>'
-		); */
+		/* return new Response('<pre>'.print_r($request->request->all()).'</pre>'); */
 		/* return $this->json([
 			'email' => $request->request->get('email'),
 			'password' => $request->request->get('password'),
