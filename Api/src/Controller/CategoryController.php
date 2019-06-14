1265b3d1f2b21229eb298c5e1b2d8748475049f2
Alex <alexandre.rosselange@epitech.eu>
2019-06-14 16:21:39 +0200

    add route /categories (all categories)

--- Api/src/Controller/CategoryController.php
+++ Api/src/Controller/CategoryController.php
@@ -12,31 +12,55 @@ class CategoryController extends AbstractController
 	/**
 	 * @Route("/category/{id}", name="category")
 	 */
-	public function		index(CategoryRepository $categories, $id)
+	public function		getCategory(CategoryRepository $categories, $id)
 	{
 		$cat = $categories->find($id);
 
 		$categoryArr['category'] = ['id' => $cat->getId(), 'name' => $cat->getName()];
 
-		$categoryArr['childs'] = $this->getCategories($cat, $cat->getId());
-		$categoryArr['parents'] = $this->getParentsNames($cat);
+		$categoryArr['childs'] = $this->getChildrens($cat, $cat->getId());
+		$categoryArr['parents'] = $this->getParents($cat);
 		return $this->json([
 			$categoryArr
 		]);
 	}
+	
+	/**
+	 * @Route("/categories/", name="all_categories")
+	 */
+	public function		getCategories(CategoryRepository $categories)
+	{
+		$cats = [];
+		$cat = $categories->findBy(['parent' => null]);
 
-	public function		getCategories($cat, $idStart) {
+		foreach ($cat as $key => $value) {
+			$cats[$value->getId()] = [
+				'name' => $value->getName(),
+				'sub' => $this->getChildrens($value, $value->getId())
+			];
+		}
+
+		// $categoryArr['category'] = ['id' => $cat->getId(), 'name' => $cat->getName()];
+
+		// $categoryArr['childs'] = $this->getChildrens($cat, $cat->getId());
+		// $categoryArr['parents'] = $this->getParents($cat);
+		return $this->json([
+			$cats
+		]);
+	}
+
+	public function		getChildrens($cat, $idStart) {
 		$categoryArr = [];
 		$child = $cat->getChildren();
 		$c = -1;
 		while (isset($child[++$c]))
-			$categoryArr[] = $this->getCategories($child[$c], $idStart);
+			$categoryArr[] = $this->getChildrens($child[$c], $idStart);
 		if ($idStart !== $cat->getId())
 			$categoryArr[] = ['id' => $cat->getId(), 'name' => $cat->getName()];
 		return (array_reverse($categoryArr));
 	}
 
-	public function		getParentsNames($cat) {
+	public function		getParents($cat) {
 		$categoryArr = [];
 		$parent = $cat->getParent();
 		while (isset($parent))
