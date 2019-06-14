6d10213020597a3850ef6fe9862bd0d12c3cc005
Alex <alexandre.rosselange@epitech.eu>
2019-06-14 19:51:35 +0200

    add create / update category

--- Api/src/Controller/CategoryController.php
+++ Api/src/Controller/CategoryController.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController,
+Symfony\Component\Routing\Annotation\Route,
+Symfony\Component\HttpFoundation\Request,
+App\Repository\UserRepository,
+Symfony\Component\Validator\Validator\ValidatorInterface,
+Doctrine\ORM\EntityManagerInterface,
+App\Entity\Category;
 
 use App\Repository\CategoryRepository;
 
+/**
+ * @Route("/category")
+ */
 class CategoryController extends AbstractController
 {
 	/**
-	 * @Route("/category/{id}", name="category")
+	 * @Route("/{id}", name="category", methods={"GET"})
 	 */
 	public function		getCategory(CategoryRepository $categories, $id)
 	{
@@ -30,7 +38,7 @@ class CategoryController extends AbstractController
 	}
 	
 	/**
-	 * @Route("/categories/", name="all_categories")
+	 * @Route("/", name="all_categories", methods={"GET"})
 	 */
 	public function		getCategories(CategoryRepository $categories)
 	{
@@ -43,17 +51,10 @@ class CategoryController extends AbstractController
 				'sub' => $this->getChildrens($value, $value->getId())
 			];
 		}
-
-		// $categoryArr['category'] = ['id' => $cat->getId(), 'name' => $cat->getName()];
-
-		// $categoryArr['childs'] = $this->getChildrens($cat, $cat->getId());
-		// $categoryArr['parents'] = $this->getParents($cat);
-		return $this->json([
-			$cats
-		]);
+		return $this->json([$cats]);
 	}
 
-	public function		getChildrens($cat, $idStart, &$articles = null)
+	private function	getChildrens($cat, $idStart, &$articles = null)
 	{
 		$categoryArr = [];
 		$child = $cat->getChildren();
@@ -71,7 +72,7 @@ class CategoryController extends AbstractController
 		return (array_reverse($categoryArr));
 	}
 
-	public function		getParents($cat)
+	private function	getParents($cat)
 	{
 		$categoryArr = [];
 		$parent = $cat->getParent();
@@ -83,4 +84,83 @@ class CategoryController extends AbstractController
 		}
 		return (array_reverse($categoryArr));
 	}
+
+	/**
+	 * @Route("/", name="new_category", methods={"POST"})
+	 * 
+	 * @param Request $req
+	 * @param UserRepository $urep
+	 * @param ValidatorInterface $vali
+	 * @param CategoryRepository $cateRepo
+	 * @param EntityManagerInterface $manger
+	 * @param CategoryRepository $categories
+	 * 
+	 * @return \Symfony\Component\HttpFoundation\JsonResponse
+	 */
+	public function		addCategoty(
+		Request $req,
+		UserRepository $urep,
+		ValidatorInterface $vali,
+		EntityManagerInterface $manger,
+		CategoryRepository $categories
+	) {
+		$token = $req->headers->get('token');
+		// if (!($user = $this->isAdmin($token, $urep)))
+		// 	return ($resp->setStatusCode(500)->setContent("bad Request"));
+		$cate = new Category();
+		
+		$cate->setName($req->request->get('name'));
+		if ($req->request->get('parent'))
+			$cate->setParent($categories->find($req->request->get('parent')));
+		$error = $vali->validate($cate);
+		if (count($error) !== 0)
+			return ($this->json($error, 400));
+		$manger->persist($cate);
+		$manger->flush();
+		return ($this->json($cate->getName()));
+	}
+
+	/**
+	 * @Route("/upd/{id}", name="upd_category", methods={"POST"})
+	 * 
+	 * @param Category $cate;
+	 */
+	public function		updCategory(
+		Category $cate,
+		Request $req,
+		UserRepository $urep,
+		ValidatorInterface $valid,
+		EntityManagerInterface $manger,
+		CategoryRepository $categories,
+		$id
+	) {
+		$token = $req->headers->get('token');
+		// if (!($user = $this->isAdmin($token, $urep)))
+		// 	return ($resp->setStatusCode(500)->setContent("bad Request"));
+
+		if ($req->request->get('parent') && $req->request->get('parent') !== $cate->getId())
+			$cate->setParent($categories->find($req->request->get('parent')));
+
+		if ($req->request->get('name'))
+			$cate->setName($req->request->get('name'));
+
+		$manger->persist($cate);
+		$manger->flush();
+
+		return $this->json([$cate->getId(), $cate->getName(), $cate->getParent()->getId()]);
+	}
+
+	/**
+	 * @param $token
+	 * @param UserRepository $uRep
+	 * @return \App\Entity\User|null
+	 */
+	/* private function    isAdmin($token, UserRepository $uRep)
+	{
+		$user = $uRep->findOneBy(['token' => $token]);
+		if (!$user || $user->isAdmin() !== true)
+			return (null);
+		return ($user);
+
+	} */
 }
