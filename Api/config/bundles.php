7ddf3e49b89ac624729014f6dc01555df685d4dc
snoy <stav.noy@epitech.eu>
2019-06-16 22:41:12 +0200

    Fixed bundle and composer dependency from branch switch

--- Api/config/bundles.php
+++ Api/config/bundles.php
@@ -12,4 +12,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
