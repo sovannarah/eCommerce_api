1a14928a680d0d55775208cbecafe00963fcfd82
skarma <amaury1.feron@epitech.eu>
2019-06-17 14:28:10 +0200

    full dynamique research

--- Api/src/Controller/SearchController.php
+++ Api/src/Controller/SearchController.php
@@ -23,11 +23,15 @@ class SearchController extends AbstractController
 	public function     Search(Request $request, ArticleRepository $tArticle)
 	{
 		$gloriusQuest = $request->query->all();
-		$tCheck = ['priceMin', 'priceMax','title', 'description', 'model', 'category0'];
+		$tcheck2 = ['priceMin', 'priceMax', 'title', 'description', 'model', 'category0'];
+		$tQuery = [
+			['priceMin', 'a.price > :priceMin'],
+			['priceMax', 'a.price < :priceMax'],['title', 'a.title like :title'],
+			['description', 'a.description like :description'], ['model', '']];
 		$findValue = false;
 		foreach ($gloriusQuest as $quest => $value)
 		{
-			if (in_array($quest, $tCheck) === true)
+			if (in_array($quest, $tcheck2) === true)
 			{
 				$findValue = true;
 				break;
@@ -37,48 +41,65 @@ class SearchController extends AbstractController
 			return ($this->json($tArticle->findAll()));
 		else
 			return ($this->json($this->initSearchAnd([ $gloriusQuest, $quest ],
-									$tCheck, $tArticle)));
+				$tQuery, $tArticle)));
 	}
 
-	private function initSearchAnd($quest, $tCheck, ArticleRepository $rAtcicle)
+	private function    initSearchAnd($quest, $tQuery ,ArticleRepository $rAtcicle)
 	{
 		$key = $quest[1];
 		$tval = $quest[0];
 		$tValCate = [];
 		$n = 0;
+		$c = -1;
+		$lentQ = count($tQuery);
 		foreach ($tval as $keys => $val)
 		{
-			if ($key . $n === 'category' . $n)
+			echo ("</br>");
+			if ($keys === 'category' . $n)
 			{
-				$tValCate[] = $val;
+				$tValCate[$n] = $val;
 				$n++;
 			}
 		}
 		$query = $rAtcicle->createQueryBuilder('a');
-		if (isset($tval['title']))
+		while (++$c < $lentQ)
 		{
-			$query->andWhere('a.title like :title');
-			$query->setParameter('title', $tval['title'] . '%');
+			if (isset($tval[$tQuery[$c][0]]))
+			{
+				$query->andWhere($tQuery[$c][1]);
+				if ($tQuery[$c][0] === 'title')
+					$query->setParameter($tQuery[$c][0], $tval[$tQuery[$c][0]] . '%');
+				else if ($tQuery[$c][0] === 'description')
+					$query->setParameter($tQuery[$c][0], '%' . $tval[$tQuery[$c][0]] . '%');
+				else
+					$query->setParameter($tQuery[$c][0], $tval[$tQuery[$c][0]]);
+			}
 		}
+
+//		if (isset($tval['title']))
+//		{
+//			$query->andWhere('a.title like :title');
+//			$query->setParameter('title', $tval['title'] . '%');
+//		}
 		if (count($tValCate) > 0)
-			$query->andWhere($query->expr()->in('id_category', $tValCate));
-		if (isset($tval['describtion']))
-		{
-			$query->andWhere('a.description like :description');
-			$query->setParameter('description', $tval['description']);
-		}
-//		if (isset($val['model']))
-//			$query->andWhere('a.id_model = :model';
-		if (isset($tval['priceMin']))
-		{
-			$query->andWhere('a.price > :priceMin');
-			$query->setParameter('priceMin', $tval['priceMin']);
-		}
-		if (isset($tval['priceMax']))
-		{
-			$query->andWhere('a.price < :priceMax');
-			$query->setParameter('priceMax', $tval['priceMax']);
-		}
+			$query->where($query->expr()->in('a.category', $tValCate));
+//		if (isset($tval['describtion']))
+//		{
+//			$query->andWhere('a.description like :description');
+//			$query->setParameter('description', $tval['description']. '%');
+//		}
+////		if (isset($val['model']))
+////			$query->andWhere('a.id_model = :model';
+//		if (isset($tval['priceMin']))
+//		{
+//			$query->andWhere('a.price > :priceMin');
+//			$query->setParameter('priceMin', $tval['priceMin']);
+//		}
+//		if (isset($tval['priceMax']))
+//		{
+//			$query->andWhere('a.price < :priceMax');
+//			$query->setParameter('priceMax', $tval['priceMax']);
+//		}
 		$art = $query->getQuery();
 		$tmp = $art->execute();
 		return ($tmp);
